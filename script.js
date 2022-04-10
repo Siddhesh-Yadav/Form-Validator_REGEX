3e5773958252cb6467a96e9a6397e624d3986909
Siddhesh Murlidhar Yadav <92988145+Siddhesh-Yadav@users.noreply.github.com>
2022-04-10 09:23:15 +0530

    Removed console logs

--- script.js
+++ script.js
@@ -29,11 +29,9 @@ username.addEventListener("blur", () => {
   let str = username.value;
   let regex = /^[a-z\s]{2,15}[0-9]+$/i;
   if (regex.test(str)) {
-    console.log("match hua");
     validName = true;
     nameErr.style.visibility = "hidden";
   } else {
-    console.log("match nhi hua");
     nameErr.style.visibility = "visible";
     validName = false;
   }
@@ -43,11 +41,9 @@ phone.addEventListener("blur", () => {
   let str = phone.value;
   let regex = /[0-9]{10}/;
   if (regex.test(str)) {
-    console.log("no match hua");
     validPhone = true;
     phoneErr.style.visibility = "hidden";
   } else {
-    console.log("no match nhi hua");
     phoneErr.style.visibility = "visible";
     validPhone = false;
   }
@@ -57,11 +53,9 @@ email.addEventListener("blur", () => {
   let str = email.value;
   let regex = /^([\w\d\-]{2,64})@([\w\d\-]+)\.(\w){2,8}(\.(\w){2,8})*$/;
   if (regex.test(str)) {
-    console.log("email match hua");
     validEmail = true;
     emailErr.style.visibility = "hidden";
   } else {
-    console.log("email match nhi hua");
     emailErr.style.visibility = "visible";
     validEmail =false;
   }
@@ -71,18 +65,15 @@ password.addEventListener("blur", () => {
   let str = password.value;
   let regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W]).{8,}/;
   if (regex.test(str)) {
-    console.log("passwod match hua");
     validPassword = true;
     passwordErr.style.visibility = "hidden";
   } else {
-    console.log("password match nhi hua");
     passwordErr.style.visibility = "visible";
     validPassword = false;
   }
 });
 function butt() {
     if (validPassword && validEmail && validPhone && validName) {
-        console.log("true hai");
     buttonn.style.border = '1px solid rgba(255, 255, 255, 0.2)';
     submit.style.transform = "translate(0, 1.3rem)";
     setTimeout(() => {
